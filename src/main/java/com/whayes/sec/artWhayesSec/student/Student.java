af146f89e5a0758c630e10252db09de1ee8ea254
Wanda Hayes <whayeswsfg@gmail.com>
2020-10-11 12:24:06 -0400

    initial commit of apis with security Wanda

---
+++ src/main/java/com/whayes/sec/artWhayesSec/student/Student.java
@@ -0,0 +1,20 @@
+package com.whayes.sec.artWhayesSec.student;
+
+public class Student {
+    private final Integer studentID;
+    private final String studentName;
+
+    public Student(Integer studentID,
+                   String studentName) {
+        this.studentID = studentID;
+        this.studentName = studentName;
+    }
+
+    public Integer getStudentID() {
+        return studentID;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+}
